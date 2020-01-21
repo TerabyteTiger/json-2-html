ba98555330998f59cd8c872124951b10134d0f45
12vanblart <12vanblart@gmail.com>
2020-01-21 16:19:56 -0500

    🌌 Add Test Space - Process Arrays

--- src/components/JsonInput.vue
+++ src/components/JsonInput.vue
@@ -1,20 +1,29 @@
 <template>
-  <form action @submit.prevent="jsonValidate">
-    <label for="jsonInput">JSON Format:</label>
-    <textarea
-      name="jsonInput"
-      id="jsonInput"
-      cols="30"
-      rows="10"
-      v-model="jsonData"
-    ></textarea>
+  <div>
+    <form action @submit.prevent="jsonValidate">
+      <label for="jsonInput">JSON Format:</label>
+      <textarea
+        name="jsonInput"
+        id="jsonInput"
+        cols="30"
+        rows="10"
+        v-model="jsonData"
+      ></textarea>
+      <input type="submit" value="Code, Please!" />
 
-    <input type="submit" value="Code, Please!" />
-
-    <pre style="text-align: left;">
+      <pre style="text-align: left;">
       {{ processed }}
-    </pre>
-  </form>
+    </pre
+      >
+    </form>
+    <hr />
+    <!-- Test Space 🌌 -->
+    <form action="">
+      <!-- START TEST SPACE -->
+
+      <!-- END TEST SPACE -->
+    </form>
+  </div>
 </template>
 
 <script>
@@ -41,9 +50,13 @@ export default {
 
         // And I loooop!
         for (const item in keys) {
-          console.error(typeof values[item]);
+          // Strings
           typeof values[item] === "string"
             ? this.processString(keys[item], values[item])
+            : null;
+          // Array
+          typeof values[item] === "object"
+            ? this.processObject(keys[item], values[item])
             : null;
         }
       } catch (err) {
@@ -53,9 +66,27 @@ export default {
     processString: function(key, value) {
       this.processed =
         this.processed +
-        `<label for="${key}">${key}</label>\n` +
+        `<label for="${key}">${key}:</label>\n` +
         `<br/>\n` +
         `<input type="text" id="${key}" placeholder="${value}"/>\n`;
+    },
+    processObject: function(key, value) {
+      if (value[0]) {
+        // Array => Select
+        this.processed =
+          this.processed +
+          `<label for="${key}">${key}:</label>\n` +
+          `<br/>\n` +
+          `<select id="${key}">\n`;
+        for (const item in value) {
+          this.processed =
+            this.processed +
+            `\t<option value="${value[item]}">${value[item]}</option>\n`;
+        }
+        this.processed = this.processed + `</select>`;
+      } else {
+        console.log("Object", key, value);
+      }
     }
   }
 };

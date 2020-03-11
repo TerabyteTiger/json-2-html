e813a6b595c6ea9b5dc53cd9193d0bf29af28e4e
12vanblart <12vanblart@gmail.com>
2020-03-11 13:51:05 -0400

    ✨ Custom Fonts & Header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,20 @@
 <template>
   <div id="app">
     <GithubRibbon />
+    <h1 class="no-m-bottom">
+      JSON-2-HTML
+    </h1>
+    <p class="no-m-top">
+      <b>
+        A quick HTML form generator - made by
+        <a
+          href="https://tyler.vanblargan.dev"
+          class="handwriting"
+          >Tyler V</a
+        >
+        ✨
+      </b>
+    </p>
     <json-input />
   </div>
 </template>
@@ -20,7 +34,9 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  /* font-family: "Avenir", Helvetica, Arial, sans-serif; */
+  font-family: "Baloo 2", "Avenir", Helvetica, Arial,
+    sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -31,5 +47,21 @@ export default {
 
 body {
   background-color: hsl(280, 10%, 95%);
+}
+
+.no-m-bottom {
+  margin-bottom: 0;
+}
+
+.no-m-top {
+  margin-top: 0;
+}
+
+.handwriting {
+  font-family: "Pacifico", cursive;
+}
+
+a:visited {
+  color: #716799;
 }
 </style>
